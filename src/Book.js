841fa72c54a13b065df9488da02d70f2e3652d38
logic0verflow <logic0verflow90@gmail.com>
2017-11-06 10:27:37 -0700

    refactor: Create Bookshelf component and update Bookshelfs entries in App.js

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ class Book extends Component {
         <div className="book-top">
           <div
             className="book-cover"
-            style={{ width: 128, height: 193, backgroundImage: `url(${ this.props.coverURL })` }}></div>
+            style={{ width: 128, height: 193, backgroundImage: `url(${this.props.coverURL})` }}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="none" disabled>Move to...</option>
@@ -20,8 +20,8 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{ this.props.title }</div>
-        <div className="book-authors">{ this.props.authors }</div>
+        <div className="book-title">{this.props.title}</div>
+        <div className="book-authors">{this.props.authors}</div>
       </div>
     )
 
